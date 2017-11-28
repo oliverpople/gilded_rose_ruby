198fb2e89d56f9e69fd331dd279e933aed9cd0c7
oliverpople <oliverpople@gmail.com>
2017-11-28 14:57:44 +0000

    Added and passed the test. Never has to decreases the Quality Sulfuras, Hand of Ragnaros

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -32,6 +32,13 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(50)
     end
+
+    it 'never has to decreases the Quality Sulfuras, Hand of Ragnaros' do
+      items = [Item.new('Sulfuras, Hand of Ragnaros', 0, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(50)
+    end
+
   end
 end
 
@@ -40,8 +47,8 @@ end
 # - Once the sell by date has passed, Quality degrades twice as fast  /
 # - The Quality of an item is never negative  /
 # - “Aged Brie” actually increases in Quality the older it gets /
-# - The Quality of an item is never more than 50
-# - “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+# - The Quality of an item is never more than 50 /
+# - “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality /
 # - “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches;
 # Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
 #
