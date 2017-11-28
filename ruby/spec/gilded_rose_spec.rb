82e277d7b85e8aca9285f4be2410159b4c6ae04e
oliverpople <oliverpople@gmail.com>
2017-11-28 14:50:06 +0000

    Added and passed the test. The quality of any item is never more than 50

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -2,6 +2,7 @@ require  './gilded_rose.rb'
 
 describe GildedRose do
   describe '#update_quality' do
+
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
       GildedRose.new(items).update_quality
@@ -25,6 +26,12 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(1)
     end
+
+    it 'the quality of any item is never more than 50' do
+      items = [Item.new('Aged Brie', 1, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(50)
+    end
   end
 end
 
@@ -32,7 +39,7 @@ end
 #
 # - Once the sell by date has passed, Quality degrades twice as fast  /
 # - The Quality of an item is never negative  /
-# - “Aged Brie” actually increases in Quality the older it gets
+# - “Aged Brie” actually increases in Quality the older it gets /
 # - The Quality of an item is never more than 50
 # - “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
 # - “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches;
