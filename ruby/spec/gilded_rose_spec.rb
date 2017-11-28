436b6b13fd854c0b7a8c1d501640c7c6c59281e1
oliverpople <oliverpople@gmail.com>
2017-11-28 14:43:15 +0000

    Added and passed test increases the quality of aged brie the older it gets

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,45 +1,42 @@
 require  './gilded_rose.rb'
 
 describe GildedRose do
-
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+  describe '#update_quality' do
+    it 'does not change the name' do
+      items = [Item.new('foo', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq 'foo'
     end
 
     it 'doubles the rate of quality degradation once a standard item passes its sell by date' do
-      items = [Item.new("foo", 0, 2)]
-      GildedRose.new(items).update_quality()
+      items = [Item.new('foo', 0, 2)]
+      GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(0)
     end
 
     it 'doesn\'t allow standard items to have negative quality values' do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      items = [Item.new('foo', 0, 0)]
+      GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(0)
     end
 
-
-
+    it 'increases the quality of aged brie the older it gets' do
+      items = [Item.new('Aged Brie', 1, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(1)
+    end
+  end
 end
 
-end
-
-
-
-=begin
-# Requirements to test
-
-- Once the sell by date has passed, Quality degrades twice as fast  /
-- The Quality of an item is never negative  /
-- “Aged Brie” actually increases in Quality the older it gets
-- The Quality of an item is never more than 50
-- “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
-- “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches;
-Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
-
-- “Conjured” items degrade in Quality twice as fast as normal items
-
-=end
+# # Requirements to test
+#
+# - Once the sell by date has passed, Quality degrades twice as fast  /
+# - The Quality of an item is never negative  /
+# - “Aged Brie” actually increases in Quality the older it gets
+# - The Quality of an item is never more than 50
+# - “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+# - “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches;
+# Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+#
+# - “Conjured” items degrade in Quality twice as fast as normal items
+#
